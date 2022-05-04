08cf93b7be89075bcd52f5d06afcd05fde80edd8
Maria-Latysheva <mlatysheva@gmail.com>
2022-05-04 09:33:24 +0200

    feat: add initial components and structure

--- project-management-app/src/App.tsx
+++ project-management-app/src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Projects from './components/Projects/Projects';
+import ErrorPage from './components/ErrorPage';
+import Header from './components/Header';
+import ErrorBoundary from './components/ErrorBoundary';
+import Footer from './components/Footer';
+// import { Provider } from 'react-redux'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ErrorBoundary>
+        {/* <Provider store={store}> */}
+            <Header />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="projects" element={<Projects />} />
+              <Route path="error" element={<ErrorPage />} />
+              <Route path="*" element={<ErrorPage />} />
+            </Routes>
+            <Footer />
+        {/* </Provider> */}
+      </ErrorBoundary>
     </div>
   );
 }
---
+++ project-management-app/src/components/Footer.tsx
@@ -0,0 +1,31 @@
+function Footer() {
+  return (
+    <footer className="footer">
+      <img
+        className="logo"
+        src="https://rs.school/images/rs_school_js.svg"
+        alt="logo of RS School"
+      />
+      <a href="mailto:mlatysheva@gmail.com">2022</a>
+      <a href="https://rs.school" className="rsschool">
+        The Rolling Slopes School
+      </a>
+      <div className="githubs">
+        <a href="https://github.com/mlatysheva" className="mygit">
+          mlatysheva
+        </a>
+        <a href="https://github.com/olena-web" className="mygit">
+          olena-web
+        </a>
+        <a href="https://github.com/julia-mironova" className="mygit">
+          julia-mironova
+        </a>
+        <a href="https://github.com/elMP" className="mygit">
+          elMP
+        </a>
+      </div>
+      
+    </footer>
+  );
+}
+export default Footer;
