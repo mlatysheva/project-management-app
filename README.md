98b08ee8941eebe4a8e8bcbf6caa16c283e53fb4
Maria-Latysheva <mlatysheva@gmail.com>
2022-05-05 20:25:39 +0200

    deploy: project-management-app to GitHub Pages

--- README.md
+++
@@ -1,2 +0,0 @@
-This is a React app to track the progress of an IT project.<br>
-It was developed by us, the group of students of RS School React Course.
--- project-management-app/src/components/ErrorPage.tsx
+++
@@ -1,9 +0,0 @@
-function ErrorPage() {
-  return (
-    <div className="main">
-      <h1>The requested page does not exist</h1>
-    </div>
-  );
-}
-
-export default ErrorPage;
