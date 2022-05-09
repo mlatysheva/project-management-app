bfa228298cc0c0b3ee67050528001dc7a7766dae
Yuliya Mironova <rabkevich15@gmail.com>
2022-05-09 12:42:18 +0200

    feat: login component

--- project-management-app/src/App.tsx
+++ project-management-app/src/App.tsx
@@ -1,33 +1,40 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Boards } from './components/Boards/Boards';
-import ErrorPage from './components/ErrorPage';
-import ErrorBoundary from './components/ErrorBoundary';
-import Footer from './components/Footer';
-import CreateBoard from './components/Board/CreateBoard';
-import Header from './components/Header';
-import { initialState } from './reducers/listReducer';
-import { Home } from './components/Home/Home';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Boards } from "./components/Boards/Boards";
+import ErrorPage from "./components/ErrorPage";
+import ErrorBoundary from "./components/ErrorBoundary";
+import Footer from "./components/Footer";
+import CreateBoard from "./components/Board/CreateBoard";
+import Header from "./components/Header";
+import { initialState } from "./reducers/listReducer";
+import { Home } from "./components/Home/Home";
+import Login from "./components/Login/Login";
+import Logout from "./components/Logout/Logout";
 
 function App() {
-  return (
-    <div className="App">
-      <ErrorBoundary>
-        <Router>
-          <Header />
-          <Routes>            
-            <Route path="/" element={<Home />} />
-            <Route path="/project-management-app" element={<Home />} />
-            <Route path="/boards" element={<Boards lists={initialState.lists} />} />            
-            <Route path="/create" element={<CreateBoard />} />
-            <Route path="error" element={<ErrorPage />} />
-            <Route path="*" element={<ErrorPage />} />
-          </Routes>
-          <Footer />
-        </Router>
-      </ErrorBoundary>
-    </div>
-  );
+	return (
+		<div className="App">
+			<ErrorBoundary>
+				<Router>
+					<Header />
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/login" element={<Login />} />
+						<Route path="/logout" element={<Logout />} />
+						<Route path="/project-management-app" element={<Home />} />
+						<Route
+							path="/boards"
+							element={<Boards lists={initialState.lists} />}
+						/>
+						<Route path="/create" element={<CreateBoard />} />
+						<Route path="error" element={<ErrorPage />} />
+						<Route path="*" element={<ErrorPage />} />
+					</Routes>
+					<Footer />
+				</Router>
+			</ErrorBoundary>
+		</div>
+	);
 }
 
 export default App;
