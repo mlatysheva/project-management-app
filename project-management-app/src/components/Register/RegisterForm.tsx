5e77c4450ce925b914aa11e8aa46536c339d14e4
Yuliya Mironova <rabkevich15@gmail.com>
2022-05-10 13:10:38 +0200

    feat: disabled and pattern

--- project-management-app/src/components/Register/RegisterForm.tsx
+++ project-management-app/src/components/Register/RegisterForm.tsx
@@ -6,6 +6,7 @@ import { v4 as uuid } from "uuid";
 import { useNavigate } from "react-router-dom";
 
 const unique_id = uuid();
+let dis = true;
 
 function SignupForm() {
 	const [name, setName] = useState("");
@@ -14,6 +15,17 @@ function SignupForm() {
 
 	const dispatch = useDispatch();
 	const navigate = useNavigate();
+
+	const isDisabled = () => {
+		const name = (document.getElementById("name") as HTMLInputElement).value;
+		const email = (document.getElementById("email") as HTMLInputElement).value;
+		const password = (document.getElementById("password") as HTMLInputElement)
+			.value;
+		if (name?.length > 1 && email?.length > 1 && password?.length > 1) {
+			dis = false;
+		}
+		return dis;
+	};
 
 	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
@@ -30,30 +42,46 @@ function SignupForm() {
 	};
 
 	return (
-		<form className="signup__form" onSubmit={(e) => handleSubmit(e)}>
+		<form
+			className="signup__form"
+			onSubmit={(e) => handleSubmit(e)}
+			onChange={isDisabled}
+		>
 			<h1>If you want to register 🌻:</h1>
 			<input
 				className="signup__input"
 				type="name"
 				placeholder="Name"
+				id="name"
 				value={name}
 				onChange={(e) => setName(e.target.value)}
+				pattern="[A-Za-z]{2,}"
+				title="Just latin letters, min 2 symbols"
+				required
 			/>
 			<input
 				className="signup__input"
 				type="email"
 				placeholder="Email"
+				id="email"
 				value={email}
 				onChange={(e) => setEmail(e.target.value)}
+				pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+				title="cat2cat@do.to"
+				required
 			/>
 			<input
 				className="signup__input"
 				type="password"
 				placeholder="Password"
+				id="password"
 				value={password}
 				onChange={(e) => setPassword(e.target.value)}
+				pattern="{6,}"
+				title="Put minimum 6 symbols"
+				required
 			/>
-			<button type="submit" className="signup__btn">
+			<button type="submit" className="signup__btn" disabled={dis}>
 				Register
 			</button>
 		</form>
