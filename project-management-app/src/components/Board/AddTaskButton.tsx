c961605f82689ecb6ed4f2da83e082b7121b9847
Maria-Latysheva <mlatysheva@gmail.com>
2022-05-10 16:11:47 +0200

    feat: add renderForm

--- project-management-app/src/components/Board/AddTaskButton.tsx
+++ project-management-app/src/components/Board/AddTaskButton.tsx
@@ -6,21 +6,32 @@ interface AddButtonProps {
 }
 
 export class AddButton extends React.Component<AddButtonProps> {
+  state = {
+    formOpen: false,
+  }
   
-  render() {
+  renderButton () {
     const { type } = this.props;
     const buttonTextOpasity = (type == "Add new task") ? 1 : 0.5;
     const buttonTextColor = (type == "Add new task") ? "white" : "inherit";
     const buttonBackground = (type == "Add new task") ? "inherit" : "ligthgrey";
     return (
       <div className="add-button" style={{
-        opacity: buttonTextOpasity, 
-        color: buttonTextColor, 
-        backgroundColor: buttonBackground
-      }}>
-        <Icon>add</Icon>
-        <p>{type}</p>
+          opacity: buttonTextOpasity, 
+          color: buttonTextColor, 
+          backgroundColor: buttonBackground
+        }}>
+      <Icon>add</Icon>
+      <p>{type}</p>
       </div>
-    );
+    )  
+  }
+
+  renderForm () {
+    return <p>Form</p>
+  }
+  
+  render() {    
+    return this.state.formOpen ? this.renderForm() : this.renderButton();
   }
 }
