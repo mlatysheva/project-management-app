9e49f7bdec22ccbe03c08d341f20e41c209b941c
Olena Honcharuk <gasmin07@gmail.com>
2022-05-31 00:57:11 +0300

    fix :remove any

--- src/helpersFunct/inputcolor.tsx
+++ src/helpersFunct/inputcolor.tsx
@@ -1,47 +1,49 @@
-export function applyColorLogin(e: any) {
-	let text = e.target.value;
+import { FormEvent, KeyboardEvent } from "react";
+
+export function applyColorLogin(e: FormEvent<HTMLFormElement | HTMLInputElement>) {
+	let text = (e.target as HTMLInputElement).value;
 	let isGreen = new RegExp(/[A-Za-zА-Яа-я0-9]{4}/g);
 	if (isGreen.test(text)) {
-		e.target.className = "signup__input green";
+		(e.target as HTMLInputElement).className = "signup__input green";
 	} else if (text.length > 0) {
-		e.target.className = "signup__input red";
+		(e.target as HTMLInputElement).className = "signup__input red";
 	} else {
-		e.target.className = "signup__input";
+		(e.target as HTMLInputElement).className = "signup__input";
 	}
 }
 
-export function applyColorPassword(e: any) {
-	let text = e.target.value;
+export function applyColorPassword(e: FormEvent<HTMLFormElement | HTMLInputElement>) {
+	let text = (e.target as HTMLInputElement).value;
 	let isGreen = new RegExp(/[A-Za-zА-Яа-я0-9]{6}/g);
 	if (isGreen.test(text)) {
-		e.target.className = "signup__input green";
+		(e.target as HTMLInputElement).className = "signup__input green";
 	} else if (text.length > 0) {
-		e.target.className = "signup__input red";
+		(e.target as HTMLInputElement).className = "signup__input red";
 	} else {
-		e.target.className = "signup__input";
+		(e.target as HTMLInputElement).className = "signup__input";
 	}
 }
 
-export function applyColorPasswordShow(e: any) {
-	let text = e.target.value;
+export function applyColorPasswordShow(e: FormEvent<HTMLFormElement | HTMLInputElement>) {
+	let text = (e.target as HTMLInputElement).value;
 	let isGreen = new RegExp(/[A-Za-zА-Яа-я0-9]{6}/g);
 	if (isGreen.test(text)) {
-		e.target.className = "password__input green";
+		(e.target as HTMLInputElement).className = "password__input green";
 	} else if (text.length > 0) {
-		e.target.className = "password__input red";
+		(e.target as HTMLInputElement).className = "password__input red";
 	} else {
-		e.target.className = "password__input";
+		(e.target as HTMLInputElement).className = "password__input";
 	}
 }
 
-export function applyColorName(e: any) {
-	let text = e.target.value;
+export function applyColorName(e: FormEvent<HTMLFormElement | HTMLInputElement>)  {
+	let text = (e.target as HTMLInputElement).value;
 	let isGreen = new RegExp(/[A-Za-zА-Яа-я]{2}/g);
 	if (isGreen.test(text)) {
-		e.target.className = "signup__input green";
+		(e.target as HTMLInputElement).className = "signup__input green";
 	} else if (text.length > 0) {
-		e.target.className = "signup__input red";
+		(e.target as HTMLInputElement).className = "signup__input red";
 	} else {
-		e.target.className = "signup__input";
+		(e.target as HTMLInputElement).className = "signup__input";
 	}
 }
