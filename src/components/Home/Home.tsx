28fd432a1cf852f6c4801cab96e34e137ed8629b
mlatysheva <mlatysheva@gmail.com>
2022-09-05 15:54:55 +0200

    feat: fix design

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -20,37 +20,57 @@ export function Home() {
 			<Title title={t('title')}/>
 			<Elefant />
 			<div className="main_container">
-				<div className="advantage">
-					<h2>{t('h2')}</h2>
-					<p className='welcome'>{t('welcome1')}</p>
-					<p className='welcome'>{t('welcome2')}</p>
-				</div>
-				<div className="advantage">
-					<h2>{t('h2_plan')}</h2>
-					<p className='welcome'>{t('welcome3')}</p>
-			  </div>
-				<div className="advantage">
-					<h2>{t('h2_use')}</h2>
-					<p className='welcome'>{t('welcome4')}</p>
-				</div>
-			</div>
-			<h2>{t('h2_made')} React 2022 Q1 in <a href="https://rs.school" className="rsschool">
-        The Rolling Scopes School
-      </a> {t('team')}</h2>
-			<div className="welcome-team">
-				<div className='developer'>
-					<img src={process.env.PUBLIC_URL + "/assets/img/Julia.jpg"} alt="Julia" />
-					<span>{t('name1')}</span>
-				</div>
-				<div className='developer'>
-					<img src={process.env.PUBLIC_URL + "/assets/img/Maria.jpg"} alt="Maria" />
-					<span>{t('name2')}</span>
-				</div>
-				<div className='developer'>
-					<img src={process.env.PUBLIC_URL + "/assets/img/Lena.jpg"} alt="Olena" />
-					<span>{t('name3')}</span>
-				</div>
-			</div>
+        <h2>{t('h2')}</h2>
+        <div className="quote_container">
+          <div className="quote">{t('quote')}</div>
+          <div className="author">{t('author')}</div>
+        </div>        
+        <div className="advantages_container">
+          <div className="advantage">
+            <p className='welcome'>{t('welcome1')}</p>
+          </div>
+          <div className="advantage">
+            <p className='welcome'>{t('welcome3')}</p>
+          </div>
+          <div className="advantage">
+            <p className='welcome'>{t('welcome4')}</p>
+          </div>
+        </div>
+        <div className="instructions-container">
+          <h2>{t('h2_instructions')}</h2>
+          <div className='instructions'>
+            <div className='instruction'>
+              <p className='instruction-text'>{t('instruction1')}</p>
+              <img className="instruction-image" src={process.env.PUBLIC_URL + "/assets/instructions/signin.jpg"} alt="signup" />
+            </div>
+            <div className='instruction'>
+              <p className='instruction-text'>{t('instruction2')}</p>
+              <img className="instruction-image" src={process.env.PUBLIC_URL + "/assets/instructions/addboard.jpg"} alt="create a board" />
+            </div>
+            <div className='instruction'>
+              <p className='instruction-text'>{t('instruction3')}</p>
+              <img className="instruction-image" src={process.env.PUBLIC_URL + "/assets/instructions/yourboards.jpg"} alt="your boards" />
+            </div>
+          </div>
+        </div>
+        <h2>{t('h2_made')} React 2022 Q1 <a href="https://rs.school" className="rsschool">
+          Rolling Scopes School
+        </a> </h2>
+        <div className="welcome-team">
+          <div className='developer'>
+            <img src={process.env.PUBLIC_URL + "/assets/img/Maria.jpg"} alt="Maria" />
+            <p>{t('developer1')}, {t('role1')}</p>
+          </div>
+          <div className='developer'>
+            <img src={process.env.PUBLIC_URL + "/assets/img/Lena.jpg"} alt="Olena" />
+            <p>{t('developer2')}, {t('role2')}</p>
+          </div>
+          <div className='developer'>
+            <img src={process.env.PUBLIC_URL + "/assets/img/Julia.jpg"} alt="Julia" />
+            <p>{t('developer3')}, {t('role2')}</p>
+          </div>				
+        </div>
+      </div>
 		</div>
 	);
 }
